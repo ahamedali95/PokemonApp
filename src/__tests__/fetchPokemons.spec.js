f16f22b33c67a85fa420058b04a3096b4bd269ba
Ahamed <ahamedali95@gmail.com>
2019-11-24 15:43:01 -0500

    removed console statement and added pokemon

--- src/__tests__/fetchPokemons.spec.js
+++ src/__tests__/fetchPokemons.spec.js
@@ -1,18 +1,28 @@
 import mockAxios from '../__mocks__/axios';
 import fetchPokemons from '../api/fetchPokemons';
 
-
 describe('fetchPokemons', () => {
+  const pokemons = [
+    {
+      "name": "bulbasaur",
+      "url": "https://pokeapi.co/api/v2/pokemon/1/"
+    },
+    {
+      "name": "ivysaur",
+      "url": "https://pokeapi.co/api/v2/pokemon/2/"
+    }
+  ];
+
   it('returns an array of pokemons', async () => {
     mockAxios.get.mockImplementationOnce(() => {
-      return Promise.resolve({ data: { results: ['hello'] }});
+      return Promise.resolve({ data: { results: pokemons } });
     });
 
     const response = await fetchPokemons();
-    expect(response).toEqual(['hello']);
+    expect(response).toEqual(pokemons);
   });
 
-  it('reject', async () => {
+  it('returns an empty array if it fails to fetch pokemons', async () => {
     mockAxios.get.mockImplementationOnce(() => {
       return Promise.reject([]);
     });
